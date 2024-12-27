859598e7ced6c85cdb541cfb96f75c4713dcbae8
Ali <alikaya4275@gmail.com>
2024-12-27 14:50:56 +0300

    AspNet User Tablosunun Özelleştirilmesi

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class AppUser :IdentityUser<int>
+    {
+        //Bu class içerisine eklenen bilgiler AspNetUsers tablona eklenecektir.
+        //Context sınıfına dbset olarak eklenmesine gerek yok. Buraya eklemek yeterli
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
