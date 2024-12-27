2d4185a75d8ac0da9aea4406f35485afb5f345eb
Ali <alikaya4275@gmail.com>
2024-12-27 15:46:27 +0300

    Interface ve Repositorylerin Yazılması

---
+++ EasyCashIdentityProject.DataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
@@ -0,0 +1,14 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Abstract
+{
+    public interface ICustomerAccountProcessDal : IGenericDal<CustomerAccountProcess>
+    {
+        //CustomerAccountProcess için özel metotlar yazmak için kullanıyoruz. Eğer özel metodu yoksa IGenericDal üzerinden gelen default metotlar kullanılacak.
+    }
+}
